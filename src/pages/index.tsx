0ba06edab14d5c9a1fc6026ecfdd1a1434e686b8
yunsu <than4502@gmail.com>
2023-05-06 23:39:31 +0900

    feat: 메인페이지 타이틀 추가
    구글 폰트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
-import { DarkmodeSwitch } from "@/components/Switch";
-import { Inter } from "next/font/google";
+import { Noto_Sans_KR, Varela_Round } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "900"],
+});
+
+const varelaRound = Varela_Round({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--varelaRound",
+});
 
 const Home = () => {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`min-h-screen py-8 px-4 ${notoSansKr.className} ${varelaRound.className}`}
     >
-      <DarkmodeSwitch />
+      <h1 className="nfont-varelaRound leading-none">
+        <span className="text-3xl -ml-0.5">{"Frontend Developer"}</span>
+        <br />I am interested in blockchain now.
+      </h1>
+      <div className="flex justify-center p-4">
+        <div className="bg-slate-400 rounded-full w-40 h-40" />
+      </div>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,8 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        varelaRound: ["var(--varelaRound)"],
       },
     },
   },
