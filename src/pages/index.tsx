aa139b7f403dd24cad60dde4b3795e136a8be104
yunsu <than4502@gmail.com>
2023-05-11 01:41:05 +0900

    feat: 3d icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Noto_Sans_KR, Varela_Round } from "next/font/google";
+import Image from "next/image";
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -14,7 +15,7 @@ const varelaRound = Varela_Round({
 const Home = () => {
   return (
     <main
-      className={`min-h-screen py-8 px-4 ${notoSansKr.className} ${varelaRound.className}`}
+      className={`min-h-screen p-8 ${notoSansKr.className} ${varelaRound.className}`}
     >
       <h1 className="nfont-varelaRound leading-none">
         <span className="text-3xl -ml-0.5">{"Frontend Developer"}</span>
@@ -22,6 +23,24 @@ const Home = () => {
       </h1>
       <div className="flex justify-center p-4">
         <div className="bg-slate-400 rounded-full w-40 h-40" />
+      </div>
+      <div className="w-full grid grid-cols-2 gap-8">
+        <div className="flex justify-center p-8 bg-yellow-200/90 rounded-2xl">
+          <Image
+            src="/images/megaphone-dynamic-color.png"
+            width="100"
+            height="100"
+            alt="megaphone"
+          />
+        </div>
+        <div className="flex justify-center p-8 bg-yellow-200/90 rounded-2xl">
+          <Image
+            src="/images/sheild-dynamic-gradient.png"
+            width="100"
+            height="100"
+            alt="megaphone"
+          />
+        </div>
       </div>
     </main>
   );
