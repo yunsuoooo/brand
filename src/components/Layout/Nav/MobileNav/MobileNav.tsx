f276b0246b17a6ba4a11c64d1983a5dcabcf2df5
yunsu <than4502@gmail.com>
2023-05-17 22:23:29 +0900

    feat: 모바일 네비게이션 다크모드 테마 적용

--- src/components/Layout/Nav/MobileNav/MobileNav.tsx
+++ src/components/Layout/Nav/MobileNav/MobileNav.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 import { cls } from "@/utils/tailwindUtil";
 import { DarkmodeSwitch } from "@/components/Switch";
+import useStore from "@/store/useStore";
 
 interface MobileNavProps {
   height: number;
 }
 
 const MobileNav = ({ height }: MobileNavProps) => {
+  const { isDarkTheme } = useStore();
+
   return (
     <div
       className={cls(
@@ -15,47 +18,69 @@ const MobileNav = ({ height }: MobileNavProps) => {
         `h-${height}`
       )}
     >
-      <div className="transition flex flex-col items-center text-primary-dark dark:text-slate-200">
+      <button
+        type="button"
+        className="transition flex flex-col items-center text-primary-dark dark:text-slate-200"
+      >
         <Image
-          src="/images/boy-front-gradient.png"
+          src={`/images/${
+            isDarkTheme ? "boy-front-gradient" : "boy-front-color"
+          }.png`}
           width="40"
           height="40"
           alt="boy-icon"
         />
         <span className="text-center text-xs">Profile</span>
-      </div>
-      <div className="transition flex flex-col items-center text-primary-dark dark:text-slate-200">
+      </button>
+      <button
+        type="button"
+        className="transition flex flex-col items-center text-primary-dark dark:text-slate-200"
+      >
         <Image
-          src="/images/computer-front-gradient.png"
-          width="40"
-          height="40"
-          alt="home-icon"
-        />
-        <span className="text-center text-xs">Project</span>
-      </div>
-      <div className="transition bg-white dark:bg-primary-dark p-3 rounded-full">
-        <div className="transition bg-primary-dark h-min p-2.5 rounded-full dark:bg-white">
-          <DarkmodeSwitch size={30} />
-        </div>
-      </div>
-      <div className="transition flex flex-col items-center text-primary-dark dark:text-slate-200">
-        <Image
-          src="/images/file-text-front-gradient.png"
+          src={`/images/${
+            isDarkTheme ? "file-text-front-gradient" : "file-text-front-color"
+          }.png`}
           width="40"
           height="40"
           alt="home-icon"
         />
         <span className="text-center text-xs">Resume</span>
+      </button>
+
+      <div className="transition bg-white dark:bg-primary-dark p-3 rounded-full">
+        <div className="transition bg-primary-dark h-min p-2.5 rounded-full dark:bg-white">
+          <DarkmodeSwitch size={30} />
+        </div>
       </div>
-      <div className="transition flex flex-col items-center text-primary-dark dark:text-slate-200">
+
+      <button
+        type="button"
+        className="transition flex flex-col items-center text-primary-dark dark:text-slate-200"
+      >
         <Image
-          src="/images/at-front-gradient.png"
+          src={`/images/${
+            isDarkTheme ? "computer-front-gradient" : "computer-front-color"
+          }.png`}
+          width="40"
+          height="40"
+          alt="home-icon"
+        />
+        <span className="text-center text-xs">Project</span>
+      </button>
+      <button
+        type="button"
+        className="transition flex flex-col items-center text-primary-dark dark:text-slate-200"
+      >
+        <Image
+          src={`/images/${
+            isDarkTheme ? "at-front-gradient" : "at-front-color"
+          }.png`}
           width="40"
           height="40"
           alt="home-icon"
         />
         <span className="text-center text-xs">Contact</span>
-      </div>
+      </button>
     </div>
   );
 };
