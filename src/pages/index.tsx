2993d36192f1a6683c9daf5ab04b76ba1b28c3f1
yunsu <than4502@gmail.com>
2024-02-05 23:14:42 +0900

    feat: Add react-threejs-fiber

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps, NextPage } from "next";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import Image from "next/image";
+
 import { getPosts } from "./api/getPosts";
 import { TPost } from "@/types";
 
@@ -24,6 +27,19 @@ const Home: NextPage<{ posts: TPost[] }> = ({ posts }: { posts: TPost[] }) => {
           <p key={id}>{title}</p>
         ))}
       </div>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </>
   );
 };
@@ -40,3 +56,29 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 export default Home;
+
+function Box(props: any) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) =>
+    meshRef.current ? meshRef.current.rotation.x : (0 += delta)
+  );
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
