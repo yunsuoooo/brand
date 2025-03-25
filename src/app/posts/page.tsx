3e84d365a69214b69a785cd06e2c2a32404b356c
yunsu <than4502@gmail.com>
2025-03-26 01:48:07 +0900

    feat: Add posts page layout

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,9 @@
 function PostsPage() {
-  return <div>Posts</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full gap-16 px-4 py-16">
+      <div className="prose w-full sm:w-[480px] text-zinc-900">Posts</div>
+    </div>
+  );
 }
 
 export default PostsPage;
